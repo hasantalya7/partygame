c98b2ae07a593e2f5919693e099063b531f30229
Kaan KAYA <kaan.kaya200@gmail.com>
2022-10-13 20:13:15 +0300

    deneme 1

--- Party game/Assets/asd.cs
+++ Party game/Assets/asd.cs
@@ -7,7 +7,7 @@ public class asd : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0;   
     }
 
     // Update is called once per frame
